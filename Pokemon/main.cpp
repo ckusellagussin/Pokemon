239ad1ab994aabee18bd73c1ab1ea1d1ca357bac
ckusellagussin <ckgussindev@googlemail.com>
2025-05-13 11:02:48 +0100

    Commit 6
    Added 2 enum list for cleaner code

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -5,17 +5,26 @@
 #define choice std::cin
 #define end '\n'
 
+enum PokemonChoice
+{
+
+    Charmander,
+    Bulbasaur,
+    Squirtle,
+    InvalidChoice
+    
+};
+
+
 int main() {
     
-    
     std::string playerName;
+    PokemonChoice Pokemon_Choice = InvalidChoice;
     msg << "What is your name?: " << end;
     std::cin >> playerName;
 
     msg << "Congratulations! You are now in the game " << playerName << "!" << end;
-
-    int tempChoice;
-    std::string playerPokemon;
+    
 
    msg << "My name is Professor Oak, I am a Pokemon expert and leader at the Pokemon research centre here" << end;
    msg << "I see you have come of age and are ready to have the selection of picking your first Pokemon!" << end;
@@ -24,36 +33,54 @@ int main() {
           "and Bulbasaur (A small green reptile with a cabbage on it's back that shoots out vines)." <<end;
     
         msg << "Which pokemon do you choose?: Please pick 1 for Charmander, 2 for Squirtle or 3 for Bulbasaur " << end;
+        int tempChoice;
         choice >> tempChoice;
-    
 
     switch (tempChoice)
     {
     case 1:
-        playerPokemon = "Charmander";
-        msg << "You have chosen Charmander!" << end;
+        Pokemon_Choice = Charmander;
         break;
         
     case 2:
-        playerPokemon = "Squirtle";
-        msg << "You have chosen Squirtle!" << end;
+        Pokemon_Choice = Squirtle;
         break;
         
     case 3:
-        playerPokemon = "Bulbasaur";
-        msg << "You have chosen Bulbasaur!" << end;
+        Pokemon_Choice = Bulbasaur;
         break;
         
     default:
-        playerPokemon = "Pikachu";
-        msg <<"You have found the secret choice because you're stubborn, have a Pikachu instead!" << end;
+        Pokemon_Choice = Charmander;
         break;
     }
 
-    msg << "Congratulations on picking your first Pokemon, " <<playerPokemon<< "! I'm sure you will become the best of friends! "
-                                                                               "It's time for you to step into the exciting world of Pokemon! "
-                                                                               "Enjoy!";
-    
+    switch(Pokemon_Choice)
+    {
+    case Charmander:
+        msg << "You have chosen Charmander!" << end;
+        msg << "Congratulations on picking your first Pokemon, Charmander! I'm sure you will become the best of friends! "
+                                                                              "It's time for you to step into the exciting world of Pokemon! "
+                                                                              "Enjoy!";
+        break;
+    case Squirtle:
+        msg << "You have chosen Squirtle!" << end;
+        msg << "Congratulations on picking your first Pokemon, Squirtle! I'm sure you will become the best of friends! "
+                                                                              "It's time for you to step into the exciting world of Pokemon! "
+                                                                              "Enjoy!";
+        break;
+    case Bulbasaur:
+        msg << "You have chosen Bulbasaur!" << end;
+        msg << "Congratulations on picking your first Pokemon, Bulbasaur! I'm sure you will become the best of friends! "
+                                                                              "It's time for you to step into the exciting world of Pokemon! "
+                                                                              "Enjoy!";
+        break;
+    default:
+            msg << "You have chosen a Pokemon on the list, let me choose for you! You have recieved Charmander since no one likes Bulbasaur!" << end;
+        
+    }
+
+
     
 }
     
