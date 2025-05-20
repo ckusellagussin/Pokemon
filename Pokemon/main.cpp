6222f7241a1a56df00347e1a4838cae36a506893
ckusellagussin <ckgussindev@googlemail.com>
2025-05-20 12:45:20 +0100

    Commit 14
    Created functions to make dialog more readable

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -2,9 +2,27 @@
 #include <string>
 #include <valarray>
 
-#define msg std::cout
-#define input std::cin
-#define end '\n'
+#define MSG std::cout
+#define INPUT std::cin
+#define END '\n'
+
+void WaitForEnter()
+{
+
+    std::cin.get();
+        
+}
+
+void clearConsole()
+{
+
+#ifdef _WIN32
+    system("cls");
+#else
+(void)system("clear");
+#endif
+    
+}
 
 enum class PokemonChoice {
 
@@ -97,33 +115,34 @@ public:
             switch ((PokemonChoice)choice)
             {   
             case PokemonChoice::Charmander:
-                msg << "You have chosen Charmander!" << end;
+                MSG << "You have chosen Charmander!" << END;
                 chosenPokemon = Pokemon("Charmander", PokemonType::Fire, 100);
-                msg << "Congratulations on picking your first Pokemon, Charmander! I'm sure you will become the best "
-                       "of friends! "
+                MSG << "Congratulations on picking your first Pokemon, Charmander! I'm sure you will become the best "
+                       "of friends! "<<std::cin.get() <<
                        "It's time for you to step into the exciting world of Pokemon! "
-                       "Enjoy!" <<end;
+                       "Enjoy!" <<END;
                 break;
             case PokemonChoice::Squirtle:
-                msg << "You have chosen Squirtle!" << end;
+                MSG << "You have chosen Squirtle!" << END;
                 chosenPokemon = Pokemon("Squirtle", PokemonType::Water, 100);
-                msg << "Congratulations on picking your first Pokemon, Squirtle! I'm sure you will become the best of "
-                       "friends! "
+                MSG << "Congratulations on picking your first Pokemon, Squirtle! I'm sure you will become the best of "
+                       "friends! " << std::cin.get() <<
                        "It's time for you to step into the exciting world of Pokemon! "
-                       "Enjoy!"<<end;
+                       "Enjoy!"<<END;
                 break;
             case PokemonChoice::Bulbasaur:
-                msg << "You have chosen Bulbasaur!" << end;
+                MSG << "You have chosen Bulbasaur!" << END;
                 chosenPokemon = Pokemon("Bulbasaur", PokemonType::Grass, 100);
-                msg << "Congratulations on picking your first Pokemon, Bulbasaur! I'm sure you will become the best of "
+                MSG << "Congratulations on picking your first Pokemon, Bulbasaur! I'm sure you will become the best of "
                        "friends! "
+               << std::cin.get() <<
                        "It's time for you to step into the exciting world of Pokemon! "
-                       "Enjoy!"<<end;
+                       "Enjoy!" <<END;
                 break;
             default:
                 chosenPokemon = Pokemon("Charmander", PokemonType::Fire, 100);
-                msg << "You have not chosen a Pokemon on the list, let me choose for you! You have recieved Charmander "
-                       "since no one likes Bulbasaur!"<< end;
+                MSG << "You have not chosen a Pokemon on the list, let me choose for you! You have recieved Charmander "
+                       "since no one likes Bulbasaur!"<< END;
             }
       
     }
@@ -145,26 +164,34 @@ public:
 
         void greetPlayer(Player& player)
         {
-            msg << "My name is Professor Oak, I am a Pokemon expert and leader at the Pokemon research centre here" << end;
-            msg << "I see you have come of age and are ready to have the selection of picking your first Pokemon!" << end;
-            msg << "You are presented with three Pokemon: They are Charmander (A fiery lizard with a flame on his tail), "
+            MSG << "My name is Professor Oak, I am a Pokemon expert and leader at the Pokemon research centre here" << END;
+            std::cin.get();
+            MSG << "I see you have come of age and are ready to have the selection of picking your first Pokemon!" << END;
+            std::cin.get();
+            MSG << "You are presented with three Pokemon: They are Charmander (A fiery lizard with a flame on his tail), "
                    "Squirtle (A small blue turtle looking pokemon with a curly tail) "
-                   "and Bulbasaur (A small green reptile with a cabbage on it's back that shoots out vines)."<< end;
+                   "and Bulbasaur (A small green reptile with a cabbage on it's back that shoots out vines)."<< END;
         }
 
         void explainMainQuest(Player& player)
         {
-
-            msg << "Professor Oak: Oak-ay "<< player.Name<< "I am about to explain to you about your upcoming grand adventure." << end;
-            msg << "Professor Oak: You see, becoming a Pokemon Master is no easy feat. It takes courage, wisdom, and a bit of luck" << end;
-            msg << "Professor Oak: Your mission, should you choose to accept it (and trust me, you really dont have a choice) is to collect all the Pokemon Badges" 
-                    "and conquer the Pokemon League. " << end;
-            msg << "Professor Oak: To achieve this, you will need to battle wild Pokemon, challenge gym leaders, and of course, keep your Pokemon healthy at the"
-                   " PokeCenter." << end;
-
-            msg << "Professor Oak: Along the way, you'll capture new Pokemon to strengthen your team. Just remember! There is a limit to how many Pokemon you"
-                    "can carry, so choose wisely!" << end;
-            msg << "Professor Oak: So, what do you say? Are you ready to become the next Pokemon Champion?" << end;
+            clearConsole();
+            MSG << "Professor Oak: Oak-ay "<< player.Name<< "! I am about to explain to you about your upcoming grand adventure." << END;
+            WaitForEnter();
+            MSG << "Professor Oak: You see, becoming a Pokemon Master is no easy feat. It takes courage, wisdom, and a bit of luck" << END;
+            std::cin.get();
+            MSG << "Professor Oak: Your mission, should you choose to accept it (and trust me, you really dont have a choice) is to collect all the Pokemon Badges" 
+                    "and conquer the Pokemon League. " << END;
+            std::cin.get();
+            MSG << "Professor Oak: To achieve this, you will need to battle wild Pokemon, challenge gym leaders, and of course, keep your Pokemon healthy at the"
+                   " PokeCenter." << END;
+            std::cin.get();
+            MSG << "Professor Oak: Along the way, you'll capture new Pokemon to strengthen your team. Just remember! There is a limit to how many Pokemon you"
+                    " can carry, so choose wisely!" << END;
+            std::cin.get();
+            MSG << "Professor Oak: So, what do you say? Are you ready to become the next Pokemon Champion?" << END;
+            std::cin.get();
+           
             
         }
         
@@ -173,20 +200,20 @@ public:
         {
             std::string playerName;
             PokemonChoice Pokemon_Choice = PokemonChoice::InvalidChoice;
-            msg << "But first off, I want to ask you the question of who will be the greatest trainer in the region: What is your name?: " << end;
+            MSG << "But first off, I want to ask you the question of who will be the greatest trainer in the region! What is your name?: " << END;
             std::getline (std::cin, player.Name);
-            msg << "Great name! Nice to meet you "<< player.Name << "! I see special things just from looking into your eyes." << end;
-        
-            msg << "Which pokemon do you choose?: Please pick 1 for Charmander, 2 for Squirtle or 3 for Bulbasaur " << end;
+            clearConsole();
+            MSG << "Great name! Nice to meet you "<< player.Name << "! I see special things just from looking into your eyes." << END;
+            std::cin.get();
+            MSG << "Which pokemon do you choose?: Please pick 1 for Charmander, 2 for Squirtle or 3 for Bulbasaur " << END;
             int tempChoice;
-            input >> tempChoice;
+            INPUT>> tempChoice;
             
             player.choosePokemon(tempChoice);
-        
+     
         }
     };
-    
-        
+
     int main()
     {
 
